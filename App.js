61b487866ec0157190b0304f6bbb9c4194fc1c9c
Cristian Ponce <cristianrponce06@gmail.com>
2018-06-01 17:03:15 -0500

    Initial Commit
    * Started project with `create-react-native-app`
    * Added necessary dependencies

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styled from 'styled-components';
+
+const ContainerView = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <ContainerView>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </ContainerView>
+    );
+  }
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,80 @@
+export const colors = {
+  PRIMARY_GREEN: '#6FAA43',
+  SECONDARY_GREEN: '#A2DD6F',
+  YELLOW: '#F6D246',
+  RED: '#FF8080',
+};
+
+export const byte = {
+  routes: [
+    {
+      duration: 101,
+      vehicleLabel: null,
+      vehicleRegistration: null,
+      driverSerial: '',
+      distance: 80.495,
+      stops: [
+        {
+          locationName: '(1132) Immersive SF',
+          scheduledAt: '08:43',
+          longitude: -122.386878,
+          address: '1050 Marin St, San Francisco, CA 94124, USA',
+          latitude: 37.74963,
+          stopNumber: 1,
+          orderNo: '',
+          locationNo: '1132',
+        },
+        {
+          locationName: '(961) Impact Hub',
+          scheduledAt: '08:57',
+          longitude: -122.419605,
+          address: '1885 Mission St, San Francisco, CA 94103, USA',
+          latitude: 37.766869,
+          stopNumber: 2,
+          orderNo: '',
+          locationNo: '961',
+        },
+      ],
+      driverName: 'Art Tkachenko',
+      load1: 0,
+      load3: 0,
+      load2: 0,
+      load4: 0,
+    },
+    {
+      duration: 21,
+      vehicleLabel: null,
+      vehicleRegistration: null,
+      driverSerial: '',
+      distance: 3.606,
+      stops: [
+        {
+          locationName: 'Byte - 101 Glacier Pt - C',
+          scheduledAt: '08:05',
+          longitude: -122.4900207,
+          address: '101 Glacier Point, Suite A, San Rafael, CA 94901',
+          latitude: 37.9501647,
+          stopNumber: 1,
+          orderNo: '',
+          locationNo: '576'
+        },
+        {
+          locationName: 'Byte - Suite N',
+          scheduledAt: '08:15',
+          longitude: -122.499466,
+          address: '3095 Kerner Blvd Suite N, San Rafael, CA 94901',
+          latitude: 37.9563461,
+          stopNumber: 2,
+          orderNo: '',
+          locationNo: '214',
+        },
+      ],
+      driverName: 'Lisa Clark',
+      load1: 0,
+      load3: 0,
+      load2: 0,
+      load4: 0,
+    },
+  ],
+  success: true
+};
